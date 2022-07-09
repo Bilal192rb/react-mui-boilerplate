f48faaa530c9bbffd9f01ab74c6d5a0fca654994
Bilal <bilal192rb@gmail.com>
2022-07-10 01:22:42 +0500

    feat: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import Router from './routes';
+
 function App() {
-  return <div>React MUI Boilerplate</div>;
+  return (<>
+    <Router />
+  </>);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
