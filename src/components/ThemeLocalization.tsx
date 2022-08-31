1a371886d86cdf347aa473e3d3b207bdb2881450
Bilal <bilal192rb@gmail.com>
2022-08-31 16:30:08 +0500

    feat: modified theme to connect localization

--- src/components/ThemeLocalization.tsx
+++ src/components/ThemeLocalization.tsx
@@ -4,13 +4,9 @@ import * as muiCoreLocales from '@mui/material/locale';
 import * as muiDataGridLocales from '@mui/x-data-grid/locales';
 import * as muiDatePickerLocales from 'date-fns/locale';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import {
-  MuiCoreSupportedLocales,
-  MuiDataGridSupportedLocales,
-  MuiDatePickerSupportedLocales,
-} from '../types/locales';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { useMemo } from 'react';
+import useLocales from '../hooks/useLocales';
 
 const ThemeLocalizationPropTypes = {
   children: PropTypes.node,
@@ -19,26 +15,23 @@ const ThemeLocalizationPropTypes = {
 type ThemeLocalizationTypes = InferProps<typeof ThemeLocalizationPropTypes>;
 function ThemeLocalization({ children }: ThemeLocalizationTypes) {
   const defaultTheme = useTheme();
-
-  const muiCoreLocale: MuiCoreSupportedLocales = 'enUS';
-  const muiDataGridLocale: MuiDataGridSupportedLocales = 'enUS';
-  const muiDatePickerLocale: MuiDatePickerSupportedLocales = 'enUS';
+  const { currentLang } = useLocales();
 
   const themeWithLocale = useMemo(
     () =>
       createTheme(
         defaultTheme,
-        muiCoreLocales[muiCoreLocale],
-        muiDataGridLocales[muiDataGridLocale]
+        muiCoreLocales[currentLang.systemValue.muiCoreLocale],
+        muiDataGridLocales[currentLang.systemValue.muiDataGridLocale]
       ),
-    [muiCoreLocale, muiDataGridLocale, defaultTheme]
+    [currentLang.systemValue, defaultTheme]
   );
 
   return (
     <ThemeProvider theme={themeWithLocale}>
       <LocalizationProvider
         dateAdapter={AdapterDateFns}
-        locale={muiDatePickerLocales[muiDatePickerLocale]}
+        locale={muiDatePickerLocales[currentLang.systemValue.muiDatePickerLocale]}
       >
         {children}
       </LocalizationProvider>
