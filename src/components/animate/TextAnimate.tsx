a97817d0590bbdd98e17c8ce0962d74531042e2c
Bilal <bilal192rb@gmail.com>
2022-09-12 14:54:02 +0500

    feat: added typography variant in text animation

--- src/components/animate/TextAnimate.tsx
+++ src/components/animate/TextAnimate.tsx
@@ -6,18 +6,30 @@ import { AnyObject } from '../../types';
 
 const TextAnimatePropTypes = {
   text: PropTypes.string.isRequired,
+  typography: PropTypes.oneOf([
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'subtitle1',
+    'subtitle2',
+    'body1',
+    'body2',
+  ]),
   variants: PropTypes.object,
   sx: PropTypes.object,
 };
 
 type TextAnimateTypes = InferProps<typeof TextAnimatePropTypes> | AnyObject;
 
-function TextAnimate({ text, variants, sx, ...other }: TextAnimateTypes) {
+function TextAnimate({ text, typography, variants, sx, ...other }: TextAnimateTypes) {
   return (
     <Box
       component={motion.h1}
       sx={{
-        typography: 'h1',
+        typography: typography || 'h1',
         overflow: 'hidden',
         display: 'inline-flex',
         ...sx,
